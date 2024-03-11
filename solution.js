4db87b2a6eb7d374cc58efc9d82e5c65821c05cd
stella-mumbi <stella.mumbi@student.moringaschool.com>
2024-03-11 22:39:36 +0300

    updated the codility test

--- solution.js
+++ solution.js
@@ -42,13 +42,56 @@ function solution(A, D) {
 
     return balance;
 }
-// for my testing purpose
-const A = [50, -100, 200, -150];
-const D = ["2020-01-01", "2020-02-15", "2020-05-20", "2020-08-10"];
 
-// Call  now the solution function with example arrays A and D
 
-const result = solution(A, D);
-// Log the result to the console (output will be the calculated balance below)
+// // // 1. Given A = [100, 100, 100, −10] and D = ["2020−12−31", "2020−12−22", "2020−12−03", "2020−12−29"], the function should return 230. Total income was equal to 100 + 100 + 100 − 10 = 290 and the fee was paid every month, so 290 - (5 * 12) = 230.
 
-console.log(result);  
+// const A = [100, 100, 100, -10];
+
+// const D = ["2020-12-31", "2020-12-22", "2020-12-03", "2020-12-29"];
+
+// // Call  now the solution function with example arrays A and D
+
+// const result = solution(A, D);
+// // Log the result to the console (output will be the calculated balance below)
+
+// console.log(result);  
+
+
+
+// 2. Given A = [180, -50, -25, -25] and D = ["2020−01−01", "2020−01−01", "2020−01−01", "2020−01−31"], the function should return 25. The income was equal to 180, the expenditure was equal to 100 and the fee was applied in every month except January: 180 - 100 - (5 * 11) = 25.
+
+// const A=[180,-50,-25,-25];
+// const D=["2020-01-01","2020-01-01","2020-01-01","2020-01-31"];
+// const result = solution(A, D);
+// console.log(result);  
+
+
+// 3. Given A = [1, -1, 0, -105, 1] and D = ["2020−12−31", "2020−04−04", "2020−04−04", "2020−04−14", "2020−07−12"], the function should return -164. The fee is paid every month. 1 - 1 + 0 - 105 + 1 - (5 * 12) = -164. Note that in April, even though the total cost of card payments was 106 (more than 100), there were only two payments made by card, so the fee was still applied. A transaction of value 0 is considered a positive, incoming transfer.
+
+// const A =[1,-1,0,-105,1];
+// const D = ["2020-12-31", "2020-04-04", "2020-04-04", "2020-04-14", "2020-07-12"];
+// const result = solution(A,D);
+// console.log(result);
+
+
+
+// 4. Given A = [100, 100, -10, -20, -30] and D = ["2020−01−01", "2020−02−01", "2020−02−11", "2020−02−05", "2020−02−08"], the function should return 80.
+
+// const A = [100,100,-10,-20,-30];
+// const D = ["2020-01-01", "2020-02-01", "2020-02-11", "2020-02-05", "2020-02-08"];
+// const result = solution(A,D);
+// console.log(result);
+
+
+
+// 5. Given A = [-60, 60, -40, -20] and D = ["2020−10−01", "2020−02−02", "2020−10−10", "2020−10−30"], the function should return −115.
+// Assume that:
+// N is an integer within the range [1..100];
+// each element of array A is an integer within the range [−1,000..1,000];
+// D contains strings in YYYY−MM−DD format, representing dates in the range 2020−01−01 to 2020−12−31.
+
+const A = [-60,60,-40,-20];
+const D = ["2020-10-01", "2020-02-02", "2020-10-10", "2020-10-30"];
+const result = solution(A,D);
+console.log(result);
